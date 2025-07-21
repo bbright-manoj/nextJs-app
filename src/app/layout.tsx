8fb667770490d881aaf51caf76be6ca8816220c4
bbright-bobby <bobby@billionbright.org>
2025-07-21 10:02:03 +0530

    dynamic_favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,27 +3,33 @@ import type { Metadata } from "next";
 import "../../src/index.scss";
 import { I18nProvider } from "./i18n/i18n-context";
 import { detectLanguage } from "./i18n/server";
-import '@/app/globalProvider';
+import "@/app/globalProvider";
+import DynamicFavicon from "../../src/app/(MainBody)/DynamicFavicon"; // <-- Add this line
+
 export const metadata: Metadata = {
-  title: "RupeEcom Services",
+  title: "One Step Delivery",
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const lng = await detectLanguage();
+
   return (
     <I18nProvider language={lng}>
-      <html lang='en'>
+      <html lang="en">
         <head>
-          <link rel='shortcut icon' href='/images/favicon/favicon.ico' />
-          <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;display=swap' rel='stylesheet' />
-          <link href='https://fonts.googleapis.com/css?family=Raleway&amp;display=swap' rel='stylesheet' />
+          {/* Static fallback favicon */}
+          {/* <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;display=swap"
+            rel="stylesheet"
+          />
+          <link href="https://fonts.googleapis.com/css?family=Raleway&amp;display=swap" rel="stylesheet" /> */}
         </head>
         <body>
+          <DynamicFavicon /> {/* This updates the favicon dynamically */}
           <ErrorBoundary>{children}</ErrorBoundary>
         </body>
       </html>
     </I18nProvider>
   );
 }
-
-
